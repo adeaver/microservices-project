328848313414bfe501c2974060c73fb8b649ea52
Andrew Deaver <deaver.andrew@gmail.com>
2019-05-19 23:15:56 -0700

    start symbol service

---
+++ symbol-service/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/articles/{category}/", ArticlesCategoryHandler)
+	http.ListenAndServe(":5000", r)
+}
+
+// TODO move this
+func ArticlesCategoryHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+}
