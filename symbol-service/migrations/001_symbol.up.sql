155e3c177166eddeda40fbf070870921b43c1a5d
Andrew Deaver <deaver.andrew@gmail.com>
2019-05-20 21:19:19 -0700

    Added docker compose and migrations

---
+++ symbol-service/migrations/001_symbol.up.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE symbols(
+    id uuid DEFAULT uuid_generate_v4 (),
+    name TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    market_capitalization NUMERIC,
+    sector TEXT,
+    industry TEXT,
+    exchange TEXT,
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX symbols_idx_symbol ON symbols(symbol);
